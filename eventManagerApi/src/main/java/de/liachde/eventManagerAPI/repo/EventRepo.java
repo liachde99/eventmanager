ca878e68e9ecc0bfd265183118632edc5cc45152
Julian <julian.lichtenauer1999@gmail.com>
2020-06-18 17:46:04 +0200

    go

--- eventManagerApi/src/main/java/de/liachde/eventManagerAPI/repo/EventRepo.java
+++
@@ -1,9 +0,0 @@
-package de.liachde.eventManagerAPI.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import de.liachde.eventManagerAPI.model.Event;
-
-public interface EventRepo extends CrudRepository<Event, Long>{
-
-}
