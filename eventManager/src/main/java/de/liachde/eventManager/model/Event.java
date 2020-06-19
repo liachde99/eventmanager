47e968194537421e498a9cadc8d5b58e0e7bc4ec
Julian <julian.lichtenauer1999@gmail.com>
2020-06-19 09:46:51 +0200

    pkey

--- eventManager/src/main/java/de/liachde/eventManager/model/Event.java
+++ eventManager/src/main/java/de/liachde/eventManager/model/Event.java
@@ -20,7 +20,7 @@ public class Event {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "eventId",unique = true,nullable = false)
+	@Column(name = "eventid",unique = true,nullable = false)
 	private Long eventId;
 	
 	@Column(name = "name",nullable = false)
