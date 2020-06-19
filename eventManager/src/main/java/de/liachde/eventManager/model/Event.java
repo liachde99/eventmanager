0cb33288636aeb98ce823ba009fe0e580de12fe8
Julian <julian.lichtenauer1999@gmail.com>
2020-06-19 09:38:51 +0200

    entity

--- eventManager/src/main/java/de/liachde/eventManager/model/Event.java
+++ eventManager/src/main/java/de/liachde/eventManager/model/Event.java
@@ -1,5 +1,6 @@
 package de.liachde.eventManager.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,10 +18,18 @@ public class Event {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "eventId",unique = true,nullable = false)
 	private Long eventId;
 	
+	@Column(name = "name",nullable = false)
 	private final String name;
+	
+	@Column(name = "ort",nullable = false)
 	private final String ort;
+	
+	@Column(name = "beschreibung")
 	private final String beschreibung;
+	
+	@Column(name = "open",nullable = false)
 	private final Boolean open;
 }
